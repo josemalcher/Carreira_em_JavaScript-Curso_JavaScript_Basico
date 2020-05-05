1a5dfa7c087d622a245219024b7706d1c0f3c87e
josemalcher <contato@josemalcher.net>
2020-05-05 07:06:49 -0300

    3 - Operadores Matemáticos - ok

--- 03-Operadores_Matematicos/js/scripts.js
+++ 03-Operadores_Matematicos/js/scripts.js
@@ -30,6 +30,9 @@ console.log(((4 - 2 + 2) / 2 + 2) * 3); // 12
 3 - Multiplicação, Divisão, Resto e Divisão Inteira
 4 - Adição, Subtração
 */
+
+/*
+
 console.log(n1++) // 12
 console.log(n1)   // 13
 console.log(++n2) // 5
@@ -39,4 +42,65 @@ console.log(n1--) // 13
 console.log(n1)   // 12
 console.log(--n2) // 4
 console.log(n2)   // 4
+*/
 
+/*
+// 04 - Operadores Aritméticos Resto da Divisão JavaScript
+console.log(n1 % 2); // 0
+console.log(13 % 2); // 1
+console.log(17 % 2); // 1
+console.log(18 % 2); // 0
+*/
+
+//05 - Operadores de Atribuição JavaScript
+var x1 = 20;
+var x2 = 10;
+/*
+
+//var result = n1 + 1;
+x1 = x1 + 1;
+console.log(x1); // 21
+
+x2++;
+console.log(x2); // 11
+x2 += 2;
+console.log(x2); // 13
+x2 -= 2;
+console.log(x2); //11
+x2 *= 2;
+console.log(x2); //22
+x2 /= 2;
+console.log(x2); //11
+
+*/
+/*
+
+//06 - Operadores de Comparação JavaScript
+
+console.log(2 == 2); // true
+console.log("jose" == "jose"); // true
+
+console.log(2 === "2"); // false
+
+console.log(2 === 2); // true
+console.log(2 != 3); // true
+
+console.log(2 !== "2"); // true
+
+console.log(2 > 1); // true
+console.log(2 < 1); // false
+console.log(2 <= 2); // true
+
+*/
+
+// 07 - Operadores Lógicos JavaScript
+var y1 = 12;
+console.log(typeof y1 == "number"); // true
+
+console.log(typeof y1 == "number" && y1 == 12); // true
+console.log(typeof y1 == "number" && y1 == 44); // false
+
+console.log(typeof y1 == "number" || y1 == 44); // true
+console.log(typeof y1 == "string" || y1 == 44); // false
+
+console.log(!true); // false
--- README.md
+++ README.md
@@ -181,9 +181,72 @@ console.log(n2)   // 4
 ```
 
 #### 04 - Operadores Aritméticos Resto da Divisão JavaScript
+
+```js
+console.log(n1 % 2); // 0
+console.log(13 % 2); // 1
+console.log(17 % 2); // 1
+console.log(18 % 2); // 0
+```
+
 #### 05 - Operadores de Atribuição JavaScript
+
+```js
+var x1 = 20;
+var x2 = 10;
+
+//var result = n1 + 1;
+x1 = x1 + 1;
+console.log(x1); // 21
+
+x2++;
+console.log(x2); // 11
+x2 += 2;
+console.log(x2); // 13
+x2 -= 2;
+console.log(x2); //11
+x2 *= 2;
+console.log(x2); //22
+x2 /= 2;
+console.log(x2); //11
+
+```
+
 #### 06 - Operadores de Comparação JavaScript
+
+```js
+
+console.log(2 == 2); // true
+console.log("jose" == "jose"); // true
+
+console.log(2 === "2"); // false
+
+console.log(2 === 2); // true
+console.log(2 != 3); // true
+
+console.log(2 !== "2"); // true
+
+console.log(2 > 1); // true
+console.log(2 < 1); // false
+console.log(2 <= 2); // true
+```
+
 #### 07 - Operadores Lógicos JavaScript
+
+```js
+var y1 = 12;
+console.log(typeof y1 == "number"); // true
+
+console.log(typeof y1 == "number" && y1 == 12); // true
+console.log(typeof y1 == "number" && y1 == 44); // false
+
+console.log(typeof y1 == "number" || y1 == 44); // true
+console.log(typeof y1 == "string" || y1 == 44); // false
+
+console.log(!true); // false
+
+```
+
 
 [Voltar ao Índice](#indice)
 
